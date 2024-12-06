37e097e82174a0c4b9501715384546799ac3f647
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-06 17:23:20 -0300

    feat: inicital controllers

---
+++ app/Http/Controllers/SupervisorDashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SupervisorDashboardController extends Controller
+{
+    public function index()
+    {
+        return view('supervisor.dashboard');
+    }
+}
