b692b8a14f926e49e8f961a578be75a7cfd684f9
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-06 17:20:35 -0300

    config ports on vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from 'path';  // Adicionando a importação do 'path'
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+
+    server: {
+        host: '0.0.0.0',  // Expõe o servidor para toda a rede
+        port: 5173,        // A porta que o Vite vai escutar
+     
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js"),
+        },
+    },
 });
