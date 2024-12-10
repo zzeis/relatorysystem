ecec79d12e5ddc27b84d111a8bdafb6134e9842a
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-10 10:01:12 -0300

    feat: add dashboard estagiarios

---
+++ resources/views/estagiario/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="allmid mt-10">
+
+    <a href="{{route('registro-ponto.index')}}" class="title text-center">Relogio Ponto <br>    <i class="ri-time-line text-center"></i></a>
+
+</div>
+
+@endsection
