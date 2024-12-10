6b0a142e04a1c9269e1a98a99b33116d3297f0bc
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-10 14:54:01 -0300

    feat: new visual for dashboard

--- resources/views/estagiario/dashboard.blade.php
+++ resources/views/estagiario/dashboard.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="allmid mt-10">
+    <div class="allmid w-full p-10">
+        <a href="{{ route('registro-ponto.index') }}" class="title text-center ">
+            <div class="bg-white dark:bg-gray-800 h-full hover:bg-gray-200  w-50 text-center p-10 roudend">
+                Relogio Ponto <br> <i class="ri-time-line text-center "></i>
 
-    <a href="{{route('registro-ponto.index')}}" class="title text-center">Relogio Ponto <br>    <i class="ri-time-line text-center"></i></a>
-
-</div>
-
+            </div>
+        </a>
+    </div>
 @endsection
