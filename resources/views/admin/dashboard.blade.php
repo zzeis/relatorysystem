18840060a3a43f912601076733829adbd7a309c8
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-10 10:02:55 -0300

    feat: add view dashboard admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,8 +3,18 @@
 @section('content')
     <div class="allmid mt-10">
 
-        <a href="{{ route('registro-ponto.index') }}" class="title text-center">
-            Lista Estagiarios
-        </a>
+        <div class="flex justify-between gap-10">
+            <a href="{{ route('admin.listaEstagiarios') }}" class="title text-center p-10">
+                <i class="ri-time-line"></i>
+                <br>
+                Gerenciamento
+
+            </a>
+            <a href="{{ route('admin.usuarios.criar') }}" class="title text-center p-10">
+                <i class="ri-user-add-line"></i>
+                <br>
+                Criar Usuario
+            </a>
+        </div>
     </div>
 @endsection
