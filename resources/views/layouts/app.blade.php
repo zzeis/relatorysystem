a4a2bdd6994b641c2434ecc50ea5cd3dd2913ed2
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-10 10:00:50 -0300

    fix: add ti.css and add control $slots

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.min.css">
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css','resources/css/ti.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/ti.css', 'resources/js/app.js'])
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
@@ -28,18 +28,22 @@
 
         <!-- Page Heading -->
         @if (isset($header))
-        <header class="bg-white dark:bg-gray-800 shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
+            <header class="bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
         @endif
 
         <!-- Page Content -->
         <main>
-            @yield('content') <!-- AQUI está o ponto de renderização da sua seção 'content' -->
-        </main>
-    </div>
-</body>
+            @isset($slot)
+                {{ $slot }} <!-- Renderiza conteúdo passado via slot -->
+            @else
+                @yield('content') <!-- Renderiza conteúdo passado via seção -->
+                @endif
+            </main>
+        </div>
+    </body>
 
-</html>
+    </html>
