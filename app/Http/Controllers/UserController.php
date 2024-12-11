6a62795c66b47630ddff4d1f1b40ed79f31eb528
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-11 17:26:25 -0300

    feat: new function update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Departamento;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -29,6 +30,33 @@ class UserController extends Controller
 
 
 
-        return redirect()->route('estagiario.dashboard')->with('success', 'Senha alterada com sucesso!');
+        return redirect()->route($user->nivel_acesso . '.dashboard')->with('success', 'Senha alterada com sucesso!');
+    }
+
+    public function viewUserInformations(User $user)
+    {
+
+        $departamento = Departamento::find($user->departamento_id);
+
+
+        return view('user.informations', compact('user', 'departamento'));
+    }
+
+    public function update(Request $request, User $user)
+    {
+
+        $validated = $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'cpf' => 'sometimes|required|size:11|regex:/^\d+$/|unique:users,cpf,' . $user->id,
+            'email' => 'sometimes|email|unique:users,email,' . $user->id,
+            'employee_code' => 'sometimes|required',
+            'local' => 'sometimes|required|string',
+            'departamento_id' => 'sometimes|nullable|exists:departamentos,id',
+            'nivel_acesso' => 'sometimes|in:estagiario,supervisor,admin'
+        ]);
+
+        $user->update(array_filter($validated));
+
+        return redirect()->back()->with('success', 'Usuário atualizado com sucesso');
     }
 }
