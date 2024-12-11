03e868bb9f273aaa471514352b2a27b93ad31a47
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-11 17:26:52 -0300

    fix: same route only for admin and supervisor

--- app/Http/Middleware/CheckAccessLevel.php
+++ app/Http/Middleware/CheckAccessLevel.php
@@ -5,13 +5,26 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
 
 class CheckAccessLevel
 {
-    public function handle(Request $request, Closure $next, $level)
+    public function handle($request, Closure $next, ...$levels)
     {
-        if (!auth()->check() || auth()->user()->nivel_acesso !== $level) {
-            abort(403, 'Unauthorized access');
+        $user = auth()->user();
+
+        if (!$user) {
+            return redirect()->route('login')->with('error', 'Você precisa estar logado');
+        }
+
+         // Garantir que $levels seja um array
+        $allowedLevels = collect($levels)->flatMap(function ($level) {
+            return is_string($level) ? explode(',', $level) : $level;
+        })->toArray();
+
+        // Verifica se o nível de acesso do usuário está nos níveis permitidos
+        if (!in_array($user->nivel_acesso, $allowedLevels)) {
+            return redirect()->back()->with('error', 'Acesso não autorizado');
         }
 
         return $next($request);
