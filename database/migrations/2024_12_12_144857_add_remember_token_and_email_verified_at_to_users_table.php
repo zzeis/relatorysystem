bcb5b189c506cba8873e77010f133c12ee71f3ff
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-12 14:51:11 -0300

    fix: migrate for remember token and email verified

---
+++ database/migrations/2024_12_12_144857_add_remember_token_and_email_verified_at_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->rememberToken(); // Adiciona a coluna remember_token
+            $table->timestamp('email_verified_at')->nullable(); // Adiciona a coluna email_verified_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['remember_token', 'email_verified_at']);
+        });
+    }
+};
