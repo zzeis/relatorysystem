3f5c3bed19bdad322141f025c9f1482df9174b3f
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-12 09:58:30 -0300

    feat: create notification customResetPasswordNotification

---
+++ app/Notifications/CustomResetPasswordNotification.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class CustomResetPasswordNotification extends ResetPassword
+{
+    public function toMail($notifiable)
+    {
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Redefinição de Senha')
+            ->view('emails.password-reset', [
+                'user' => $notifiable,
+                'resetUrl' => $url
+            ]);
+    }
+}
