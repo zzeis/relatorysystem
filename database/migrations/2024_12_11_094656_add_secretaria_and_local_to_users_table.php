49be37c73a7354db373fecd7b6dcdaa174e8cca7
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-12 09:20:27 -0300

    feat: new migration for secretaria,local

---
+++ database/migrations/2024_12_11_094656_add_secretaria_and_local_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('secretaria')->nullable()->after('departamento_id'); // Campo para secretaria
+            $table->string('local')->nullable()->after('secretaria'); // Campo para local
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['secretaria', 'local']);
+        });
+    }
+};
