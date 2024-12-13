f7b73694991ae19e2af3a7f552668799deb45b4f
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-13 10:04:03 -0300

    fix: add new button gerecimanetoSupervisores

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,16 +4,23 @@
     <div class="allmid mt-10">
 
         <div class="flex bg-white  dark:bg-gray-800 rounded justify-between  text-gray-500 dark:text-gray-400">
-          
-                <a href="{{ route('admin.registros-ponto-all') }}"
-                    class="title hover:bg-gray-200 text-center p-10 text-gray-500 dark:text-gray-400">
-                    <i class="ri-time-line"></i>
-                    <br>
-                    Gerenciamento
-                </a>
-           
-            <a href="{{ route('admin.usuarios.criar') }}" 
-            class="title  hover:bg-gray-200  text-center p-10 text-gray-500 dark:text-gray-400">
+
+            <a href="{{ route('admin.registros-ponto-all') }}"
+                class="title hover:bg-gray-200 text-center p-10 text-gray-500 dark:text-gray-400">
+                <i class="ri-group-line"></i>
+                <br>
+                Gerenciamento Estagiarios
+            </a>
+
+            <a href="{{ route('admin.user.supervisor') }}"
+                class="title hover:bg-gray-200 text-center p-10 text-gray-500 dark:text-gray-400">
+                <i class="ri-user-settings-line"></i>
+                <br>
+                Gerenciamento Supervisores
+            </a>
+
+            <a href="{{ route('admin.usuarios.criar') }}"
+                class="title  hover:bg-gray-200  text-center p-10 text-gray-500 dark:text-gray-400">
                 <i class="ri-user-add-line"></i>
                 <br>
                 Criar Usuario
