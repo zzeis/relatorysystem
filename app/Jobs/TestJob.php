3d6b948ae4d6d900c3f3150abe33b37c9fac6b1e
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-13 09:59:11 -0300

    feat: add TestJob

---
+++ app/Jobs/TestJob.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class TestJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        Log::info('TestJob is being processed!');
+    }
+}
