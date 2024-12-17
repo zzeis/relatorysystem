4f5e030c5b1b8fe8c0573147a8c242799dc06842
zzeis <moisesmonteiro.stark@gmail.com>
2024-12-17 15:17:32 -0300

    fix: fix colors title

--- resources/views/estagiario/dashboard.blade.php
+++ resources/views/estagiario/dashboard.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="allmid w-full p-10">
         <a href="{{ route('registro-ponto.index') }}" class="title text-center ">
-            <div class="bg-white rounded dark:bg-gray-800 h-full hover:bg-gray-200  w-50 text-center p-10 roudend">
+            <div
+                class=" text-gray-500 dark:text-gray-400 bg-white rounded dark:bg-gray-800 h-full hover:bg-gray-200  w-50 text-center p-10 roudend">
                 Relogio Ponto <br> <i class="ri-time-line text-center "></i>
 
             </div>
